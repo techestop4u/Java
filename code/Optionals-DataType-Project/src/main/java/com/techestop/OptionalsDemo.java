34950e3dc957236072acb43eb65d2becbdd952e7
Prateek <prateek@Prateeks-MacBook-Air.local>
2022-01-19 13:33:58 +0530

    Added project for Optional data type demo

---
+++ code/Optionals-DataType-Project/src/main/java/com/techestop/OptionalsDemo.java
@@ -0,0 +1,308 @@
+package com.techestop;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/**
+ * Hello world!
+ *
+ */
+public class OptionalsDemo
+{
+    public static void main( String[] args )
+    {
+
+        /*
+         ************ 1. Creating Optional object ***************
+         */
+        //Using static empty() method of Optional class.
+
+        Optional<String> optionalEmptyObj = Optional.empty();
+        //isEmpty method available from Java 11
+        System.out.println(optionalEmptyObj.isEmpty());
+
+        //Using static of() method of Optional class. The value passed to of() method whouls ne non Null
+
+        Optional<String> optionalNonEmptyObj = Optional.of("Test");
+        System.out.println(optionalNonEmptyObj);
+
+
+        //Using static ofNullable() method
+        Optional<String> optionalNullableObj = Optional.ofNullable(null);
+        System.out.println(optionalNullableObj);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+         ************ 2. Optional value checking ***************
+         */
+        /*Optional<String> optionalTestObj = Optional.of("Test");
+        System.out.println(optionalTestObj.isPresent());*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+         ************ 3. ifPresent method example ***************
+         */
+        /*Optional<String> optionalNonEmptyObj = Optional.of("Prateek");
+        optionalNonEmptyObj.ifPresent((name) -> System.out.println("Name: "+name));*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+         ************ 3. ifPresentOrElse method example ( Available from Java 9 ) ***************
+         */
+        /*Optional<String> optionalEmptyObj = Optional.empty();
+        optionalEmptyObj.ifPresentOrElse((s) -> System.out.println(s),() -> System.out.println("Value unknown"));*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+         ************ 4. orElse and orElseGet method example ***************
+         */
+        /*Optional<String> optionalNonEmptyObj = Optional.of("Prateek");
+        Optional<String> optionalEmptyObj = Optional.empty();
+        System.out.println(optionalNonEmptyObj.orElse("Hello"));
+        System.out.println(optionalEmptyObj.orElseGet(()->"Hello"));*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+         ************ 5. Get method example ***************
+         */
+        /*Optional<String> optionalNonEmptyObj = Optional.of("Prateek");
+        System.out.println(optionalNonEmptyObj.get());
+
+        //Trying to get null optional value leads to NoSuchElementException exception
+        Optional<String> optionalNullableObj = Optional.ofNullable(null);
+        optionalNullableObj.get();*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+         ************ 6. Map method example ***************
+         */
+        /*String text = "My name is Prateek";
+        Optional<String> optionalText = Optional.of(text);
+        optionalText.map((s) -> s.length()).ifPresent((l) -> System.out.println("Length of the text: "+l));*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+         ************ 7. Filter method example ***************
+         */
+        /*String text = "My name is Prateek";
+        Optional<String> optionalText = Optional.of(text);
+        System.out.println(optionalText.filter((s) -> s.contains("Prateek")).get());
+        System.out.println(optionalText.filter((s) -> s.contains("Test")).isEmpty());*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+         ************ 8. Equating 2 optional objects ***************
+         * Two optional objects are equal when
+         *  it is also an Optional and;
+         *  both instances have no value present or;
+         *  the present values are "equal to" each other via equals().
+         */
+       /* Optional<String> obj1 = Optional.of("Optional Object");
+        Optional<String> obj2 = Optional.of("Optional Object");
+        Optional<String> obj3 = Optional.of("Optional Object 3");
+        System.out.println("check if the optional object obj1 and obj2 are equal or not: "+ obj1.equals(obj2));
+        System.out.println("check if the optional object obj1 and obj3 are equal or not: "+ obj1.equals(obj3));
+        String strObj = "Optional Object";
+        System.out.println("check if the optional object obj1 and strObj are equal or not: "+ obj1.equals(strObj));*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+         ************ 9. hashCode method example ***************
+         */
+        /*Optional<String> strObj = Optional.of("Optional");
+        Optional<Integer> intObj = Optional.of(1);
+        Optional<Boolean> boolObj = Optional.of(false);
+        Optional<Integer> emptyObj = Optional.empty();
+        System.out.println(strObj.hashCode());
+        System.out.println(intObj.hashCode());
+        System.out.println(boolObj.hashCode());
+        System.out.println(emptyObj.hashCode());*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+         ************ 10. Stream method example ***************
+         * ( Introduced in Java 9 )
+         */
+        /*Optional<String> stringOptional = Optional.of("StrObj");
+        Stream<String> strObjStream = stringOptional.stream();
+        strObjStream.forEach(s -> System.out.println(s));
+        Optional<String> emptyOptional = Optional.empty();
+        Stream<String> emptyStrObjStream = emptyOptional.stream();
+        emptyStrObjStream.forEach(s -> System.out.println(s));*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- code/Streams/Streams-Project/src/main/java/com/techestop/StreamsJavaCode.java
+++ code/Streams/Streams-Project/src/main/java/com/techestop/StreamsJavaCode.java
@@ -22,14 +22,19 @@ import java.util.stream.Stream;
  * This class contains code examples related to Streams
  *
  */
-public class StreamsJavaCode
-{
-    public static void main( String[] args ) throws IOException, URISyntaxException {
+public class StreamsJavaCode {
+
+    public static void printStreamValues(String streamName, Stream s) {
+        System.out.println("Printing stream values for " + streamName);
+        s.forEach(str -> System.out.println(str));
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
         /*
-           ************* 1. Stream creation ***********************
+         ************* 1. Stream creation ***********************
          */
-        /*
-        //Creating an empty sequential stream
+        /*//Creating an empty sequential stream
         Stream<String> stringStream1 = Stream.empty();
         printStreamValues("stringStream1", stringStream1);
 
@@ -52,8 +57,8 @@ public class StreamsJavaCode
         //Creating a stream with an initial value and subsequent elements ( generated after applying the function ).
         //Lastly limiting the value to 20.
         Stream<Integer> intStream = Stream.iterate(20, n -> n + 1).limit(20);
-        printStreamValues("intStream", intStream);
-        **/
+        printStreamValues("intStream", intStream);*/
+
 
 
 
@@ -92,7 +97,9 @@ public class StreamsJavaCode
         System.out.println(result);
 
         //Using the stream again will cause IllegalStateException
-        Optional<String> result2 = stream.findAny();*/
+        stream = stringList.stream().filter((s) -> s.contains("str"));
+        Optional<String> result2 = stream.findAny();
+        System.out.println(result2);*/
 
 
 
@@ -115,34 +122,33 @@ public class StreamsJavaCode
 
         /*List<String> unmappedList = Arrays.asList("str1", "Str2", "str3");
         Stream<String> mappedStream = unmappedList.stream().map(s -> s + " value");
-        printStreamValues("mappedStream",mappedStream);
+        printStreamValues("mappedStream", mappedStream);
 
 
         IntStream intStream = IntStream.range(0, 20).skip(5);
         System.out.println("Printing values for intStream");
-        intStream.forEach( i -> System.out.println(i));
+        intStream.forEach(i -> System.out.println(i));
 
-        int sum = IntStream.range(4, 10).peek( i -> System.out.println("Integer value being processed : " + i)).sum();
+        int sum = IntStream.range(4, 10).peek(i -> System.out.println("Integer value being processed : " + i)).sum();
         // 4 + 5 + 6 + 7 + 8 + 9 = 39
         System.out.println("Sum of integers from 4 to 9 is " + sum);
 
 
         List<Integer> unsortedIntLIst = Arrays.asList(5, 9, 10, 4);
-        Optional<Integer> maxValue  = unsortedIntLIst.stream().max((a , b) -> {
-            if(a > b){
+        Optional<Integer> maxValue = unsortedIntLIst.stream().max((a, b) -> {
+            if (a > b) {
                 return 1;
-            }else if( a < b){
+            } else if (a < b) {
                 return -1;
-            }
-            else{
+            } else {
                 return 0;
             }
         });
-        System.out.println("Max Value is : "+ maxValue);
+        System.out.println("Max Value is : " + maxValue);
 
         List<Integer> dupeElementList = Arrays.asList(5, 9, 10, 4, 7, 5, 0, 9);
-        printStreamValues("distinctElementList" , dupeElementList.stream().distinct());
-        }*/
+        printStreamValues("distinctElementList", dupeElementList.stream().distinct());*/
+
 
 
 
@@ -162,13 +168,14 @@ public class StreamsJavaCode
          */
 
         /*List<String> stringList = Arrays.asList("str1", "Str2", "str3");
-        Optional<String> stream = stringList.stream().filter(element -> {
+        Optional<String> streamFirst = stringList.stream().filter(element -> {
             System.out.println("filter() was called for "+element);
             return element.contains("2");
         }).map(element -> {
             System.out.println("map() was called for "+element);
             return element.toUpperCase();
-        }).findFirst();*/
+        }).findFirst();
+        System.out.println(streamFirst);*/
 
 
 
@@ -185,7 +192,8 @@ public class StreamsJavaCode
          ************* 6. Order of Execution ***********************
          */
         /*List<String> list = Arrays.asList("String1","String2","String3");
-        long size = list.stream().skip(2).map(element -> element.substring(0, 3)).count();*/
+        long size = list.stream().map(element -> element.substring(0, 3)).peek((s) -> System.out.println("Element being processed : "+ s) ).skip(2).count();
+        System.out.println(size);*/
 
 
 
@@ -217,8 +225,8 @@ public class StreamsJavaCode
 
         List<String> stringList = Arrays.asList("Hello","This","is","Prateek");
         Stream<String> stringStream = stringList.stream();
-        System.out.println(stringStream.collect(Collectors.joining(" ","*** "," ***")));*/
-
+        System.out.println(stringStream.collect(Collectors.joining(" ","*** "," ***")));
+*/
 
 
 
@@ -258,18 +266,14 @@ public class StreamsJavaCode
          ************* 9. Flat map ***********************
          */
 
-        /*Path filePath = Paths.get("src","main","resources","FlatMapDemo");
+        Path filePath = Paths.get("src","main","resources","FlatMapDemo");
         Stream<String> lines = Files.lines(filePath, StandardCharsets.UTF_8);
         Stream<String> words = lines.flatMap(line -> Stream.of(line.split(" +")));
-        printStreamValues("words",words);*/
+        printStreamValues("words",words);
 
 
     }
 
-    public static void printStreamValues(String streamName, Stream s){
-        System.out.println("Printing stream values for "+ streamName);
-        s.forEach(str -> System.out.println(str));
-        System.out.println();
-    }
+
 }
 
