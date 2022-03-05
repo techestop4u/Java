9cfd7547658fdcfc80efc65bdddd6b7a880f47a0
Prateek <prateek@Prateeks-MacBook-Air.local>
2022-03-05 23:15:45 +0530

    Transfer queue implementation

---
+++ code/JavaConcurrentAPIs/src/transferqueue/DrainConsumer.java
@@ -0,0 +1,41 @@
+package transferqueue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.LinkedTransferQueue;
+import java.util.logging.Logger;
+
+public class DrainConsumer implements  Runnable{
+    private Logger logger = Logger.getLogger(Consumer.class.getName());
+    private String consumerName ;
+
+    DrainConsumer(String name){
+        this.consumerName = name;
+    }
+
+    @Override
+    public void run() {
+
+        LinkedTransferQueue q = (LinkedTransferQueue) TransferQueueDemo.queue;
+
+        try {
+            while(true){
+
+                Thread.sleep(5000);
+
+                    logger.info("["+consumerName+"] Trying to drain all the messages from queue.");
+                    List<String> messages = new ArrayList<>();
+                    int numOfMessagesRead = q.drainTo(messages);
+                    //Drain maximum number of messages
+                    //int numOfMessagesRead = q.drainTo(messages,2);
+                    logger.info("["+consumerName+"] Number of messages read : "+ numOfMessagesRead);
+                    logger.info("["+consumerName+"] Printing all the messages read.....");
+                    messages.stream().forEach(msg -> System.out.println(msg));
+
+
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ code/JavaConcurrentAPIs/src/transferqueue/Producer.java
@@ -0,0 +1,45 @@
+package transferqueue;
+
+import java.util.concurrent.LinkedTransferQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+public class Producer implements Runnable{
+
+    private Logger logger = Logger.getLogger(Producer.class.getName());
+    private static int counter = 0;
+
+    private String producerName ;
+
+    Producer(String name){
+        this.producerName = name;
+    }
+    @Override
+    public void run() {
+        String message;
+        LinkedTransferQueue q = (LinkedTransferQueue) TransferQueueDemo.queue;
+        try {
+            while(true) {
+                message = "TestMessage_"+(++counter);
+                logger.info("["+producerName+"] Trying to transfer the message : "+message);
+                boolean transferResult = q.tryTransfer(message, 3, TimeUnit.SECONDS);
+                logger.info("["+producerName+"] Transfer result: " + (transferResult ? "Successful" : "Failed"));
+                if(!transferResult){
+
+                    logger.info("["+producerName+"] Wait for consumers.");
+                    while(!q.hasWaitingConsumer()){
+                        Thread.sleep(3000);
+                    }
+                    logger.info("["+producerName+"] Start producing again.");
+                }else{
+                    //For simulating delay in producing the messages
+                    Thread.sleep(1000);
+                }
+
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
